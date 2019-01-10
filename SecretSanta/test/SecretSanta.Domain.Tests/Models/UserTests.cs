949fb55ca408d697e36a7e79b82a841ecbe17177
Kenneth White <kennival@hotmail.com>
2019-01-10 15:01:25 -0800

    Initial project setup

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            User user = new User { FirstName = "Inigo", LastName = "Montoya" };
+            Assert.AreEqual(user.FirstName, "Inigo");
+            Assert.AreEqual(user.LastName, "Montoya");
+        }
+    }
+}
