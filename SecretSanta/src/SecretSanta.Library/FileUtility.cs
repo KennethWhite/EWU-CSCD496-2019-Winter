053b04a4139838f4f54c0337273308b28ae6a205
Kenneth White <kennival@hotmail.com>
2019-01-21 15:30:59 -0800

    Refactored Project structure, reworked relative path in test class

---
+++ SecretSanta/src/SecretSanta.Library/FileUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace SecretSanta.Library
+{
+    public class FileUtility
+    {
+        public static FileStream OpenFile(string fileName)
+        {
+            return new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Library.Tests/FileUtility.Tests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Library;
+using System;
+using System.IO;
+
+namespace SecretSanta.Library.Tests
+{
+    [TestClass]
+    public class FileUtilityTests
+    {
+        private string ValidFilePath { get; set; } =
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, 
+                @"..\..\..\..\..\test\TestData\ValidFile.txt");
+
+        [TestMethod]
+        public void OpenFile_ValidFileName_NotNull()
+        {
+            FileStream fout = FileUtility.OpenFile(ValidFilePath);
+            Assert.IsNotNull(fout);
+            fout.Close();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void OpenFile_FileNotFound_ThrowsFNFException()
+        {
+            FileStream fout = FileUtility.OpenFile("NoFile.txt");
+            Assert.Fail();
+        }
+    }
+}
