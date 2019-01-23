6f15a44eabb832c6f614bff36045174f70ccf308
Kenneth White <kennival@hotmail.com>
2019-01-22 20:48:31 -0800

    Improved cleanup of test files

--- SecretSanta/test/SecretSanta.Library.Tests/FileUtility.Tests.cs
+++ SecretSanta/test/SecretSanta.Library.Tests/FileUtility.Tests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SecretSanta.Library;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace SecretSanta.Library.Tests
@@ -8,18 +9,31 @@ namespace SecretSanta.Library.Tests
     [TestClass]
     public class FileUtilityTests
     {
-
+        private List<string> TempFiles { get; } = new List<string>();
 
         [TestInitialize]
         public void InizializeTestFile()
         {
+            if (TempFiles.Count > 0)
+            {
+                foreach (string filePath in TempFiles)
+                {
+                    File.Delete(filePath);
+                }
+                TempFiles.Clear();
+            }
             ValidFile = Path.GetTempFileName();
+            TempFiles.Add(ValidFile);
         }
 
         [TestCleanup]
         public void CleanupTestFile()
         {
-            File.Delete(Path.GetFullPath(ValidFile));
+            foreach (string filePath in TempFiles)
+            {
+                File.Delete(filePath);
+            }
+            TempFiles.Clear();
         }
         public string ValidFile { get; set; }
 
