13d1d2b2f80635089bcebb1eaa42d420c881f55f
Kenneth White <kennival@hotmail.com>
2019-02-11 16:53:50 -0800

    Modified Seervice Interfaces to support threading

--- SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SecretSanta.Domain.Models;
 using SecretSanta.Domain.Services.Interfaces;
 
@@ -26,7 +28,7 @@ namespace SecretSanta.Domain.Services
             return gift;
         }
 
-        public Gift UpdateGiftForUser(int userId, Gift gift)
+        public Task<Gift> UpdateGiftForUser(int userId, Gift gift)
         {
             if (gift == null) throw new ArgumentNullException(nameof(gift));
 
@@ -37,9 +39,9 @@ namespace SecretSanta.Domain.Services
             return gift;
         }
 
-        public List<Gift> GetGiftsForUser(int userId)
+        public async Task<List<Gift>> GetGiftsForUser(int userId)
         {
-            return DbContext.Gifts.Where(g => g.UserId == userId).ToList();
+            return await DbContext.Gifts.Where(g => g.UserId == userId).ToListAsync();
         }
 
         public void RemoveGift(Gift gift)
