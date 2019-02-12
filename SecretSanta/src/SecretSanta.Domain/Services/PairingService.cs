df671fd8535d4de74fc763e37b24c1568a7462cb
Casey White <caseywhite534@gmail.com>
2019-02-11 19:58:38 -0800

    Implemented PairingController and PairingViewModel

--- SecretSanta/src/SecretSanta.Domain/Services/PairingService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/PairingService.cs
@@ -4,7 +4,6 @@ using SecretSanta.Domain.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SecretSanta.Domain.Services
