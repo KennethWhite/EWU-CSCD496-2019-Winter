d83545b3f999127a3b792df48273d77c75fb4ef0
Casey White <caseywhite534@gmail.com>
2019-02-26 21:10:22 -0800

    added basic group view

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ApiModels;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private IHttpClientFactory ClientFactory { get; }
+        public GroupsController(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Groups = await secretSantaClient.GetGroupsAsync();
+            }
+                return View();
+        }
+    }
+}
