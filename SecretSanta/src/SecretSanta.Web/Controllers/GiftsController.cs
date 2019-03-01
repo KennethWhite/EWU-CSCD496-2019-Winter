1e2a6d60d7ee523d6da793a1e7f38c982d89eefa
Casey White <caseywhite534@gmail.com>
2019-02-28 19:02:08 -0800

    Basic gift view and controller

--- SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ApiModels;
 
 namespace SecretSanta.Web.Controllers
 {
     public class GiftsController : Controller
     {
-        public IActionResult Index()
+        private IHttpClientFactory ClientFactory { get; }
+        private IMapper Mapper { get; }
+        public GiftsController(IHttpClientFactory clientFactory, IMapper mapper)
         {
+            ClientFactory = clientFactory;
+            Mapper = mapper;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Gifts = await secretSantaClient.GetGiftsForUserAsync(1);    //todo this is only one users gifts
+            }
             return View();
         }
     }
